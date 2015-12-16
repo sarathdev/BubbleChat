5f5fa17b0c14eda68f13e9973380e57f8536a6ee
Sarath Sasi <sarathukumbalath@outlook.com>
2015-12-16 19:03:55 +0530

    Initial Commit

---
+++ BubbleChat/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  BubbleChat
+//
+//  Created by Sarath on 16/12/15.
+//  Copyright © 2015 Sarath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ BubbleChat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BubbleChat
+//
+//  Created by Sarath on 16/12/15.
+//  Copyright © 2015 Sarath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
