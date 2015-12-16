6da37c8b5cd54dc6f3293f5ea727d2da68013612
Sarath Sasi <sarathukumbalath@outlook.com>
2015-12-16 19:40:57 +0530

    Added UITableView Method

--- BubbleChat/ViewController.h
+++ BubbleChat/ViewController.h
@@ -8,9 +8,12 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController<UITableViewDataSource,UITableViewDelegate>{
+    NSMutableArray *messages;
+    
+}
 @property (weak, nonatomic) IBOutlet UITableView *tbl;
-
-
+@property (nonatomic , retain) NSMutableArray *messages;
+ 
 @end
 
--- BubbleChat/ViewController.m
+++ BubbleChat/ViewController.m
@@ -16,12 +16,41 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    messages = [[NSMutableArray alloc]initWithObjects:@"Working Late",@"Oh too bad",@"want grab somefood for you",@"I will pick you up",@"Wnt to finish the project",@"Ok", nil];
+    UIImageView *balloonView;
+    UILabel *label;
+    
+    
+    // Do any additional setup after loading the vie w, typically from a nib.
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+-(NSInteger)numberOfSectionsInTableView:(UITableView *)tableVie{
+    return 1;
+    
+}
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    static NSString *cellIdentifier = @"Cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cellIdentifier];
+    }
+    
+    
+    return cell;
+    
+}
+-(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
+    return 1;
+    
+}
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return  messages.count;
+}
+
 
 @end
